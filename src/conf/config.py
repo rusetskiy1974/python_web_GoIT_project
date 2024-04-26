6eaf2f01eec6f0c348e594b93a04a9c6159ec472
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2024-04-26 12:34:39 +0300

    init1

---
+++ src/conf/config.py
@@ -0,0 +1,34 @@
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Settings for the application.
+    """
+    sqlalchemy_database_url: str = "postgresql+asyncpg://postgres:567234@localhost:5432/project_web"
+    db_url: str
+    db_local_url: str
+    db_admin: str
+    db_password: str
+    db_port: str
+    db_name: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    redis_host: str
+    redis_local_host: str = 'localhost'
+    redis_port: int = '6379'
+    cloudinary_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
+    cloudinary_url: str
+    max_image_size: int
+    max_add_tags: int
+
+    model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")
+
