8a90783b18f9ca91c12f95db84e1f59732236f9d
Serghii777 <142201494+Serghii777@users.noreply.github.com>
2024-05-04 06:44:57 +0200

    fix5

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -55,21 +55,6 @@ async def unblock_user_by_email(
 
     return user
 
-@router.get('/all_images', response_model=list[ImageReadSchema], dependencies=[Depends(role_admin)])
-async def get_all_images_of_all_users(email: Optional[str] = None, limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
-                                      db: AsyncSession = Depends(get_db)):
-    if email:
-        user = await get_user_by_email(email, db)
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
-        query = select(Image).filter_by(user_id=user.id).offset(offset).limit(limit)
-    else:
-        query = select(Image).offset(offset).limit(limit)
-    
-    result = await repository_images.get_images(query, db)
-    if not result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No images found.")
-    return result
 
 @router.delete('/admin/delete/{image_id}', status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(role_admin)])
 async def admin_delete_image(image_id: int = Path(..., description="The ID of the image to delete"),
@@ -93,7 +78,6 @@ async def admin_delete_image(image_id: int = Path(..., description="The ID of th
 async def update_image_by_admin(
         image_id: int = Path(..., title="The ID of the image to update"),
         title: str = Form(None, min_length=3, max_length=50, title="New title for the image (optional)"),
-        tags: list[str] = Form([], title="List of tags to update"),
         current_user: User = Depends(auth_service.get_current_user),
         db: AsyncSession = Depends(get_db)
 ):
@@ -103,7 +87,4 @@ async def update_image_by_admin(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found")
     if title is not None:
         image = await repository_images.update_image_title(image, title, db)
-    if tags:
-        await repository_images.add_tag_to_image(image, tags, db)
-
     return image
