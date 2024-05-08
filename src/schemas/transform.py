63868826de5f1c9c7965b216caaf71d9b98b03b1
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2024-05-08 14:18:59 +0300

    summary

--- src/schemas/transform.py
+++ src/schemas/transform.py
@@ -4,8 +4,12 @@ from typing import Literal
 from fastapi import Path
 from pydantic import BaseModel
 from fastapi.params import Query
+from starlette.datastructures import UploadFile
+from starlette.responses import FileResponse
 
 from src.conf.transform import TRANSFORM_METHOD
+from src.models.models import Image
+from src.schemas.image import ImageReadSchema
 
 method_list = tuple(TRANSFORM_METHOD.keys())
 
@@ -13,4 +17,8 @@ method_list = tuple(TRANSFORM_METHOD.keys())
 class TransformedImageRequest(BaseModel):
     image_id: int = Path(..., ge=1),
     method: Literal[method_list] = Query(...)  # type: ignore
+
+
+class TransformedImageResponse(BaseModel):
+    image: ImageReadSchema
 
